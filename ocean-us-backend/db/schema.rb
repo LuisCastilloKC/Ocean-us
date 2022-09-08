1787deefc5a788f8a782a1cb28e65f85fcc18b42
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-07 23:52:48 -0400

    add City model and database

--- ocean-us-backend/db/schema.rb
+++ ocean-us-backend/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_08_034938) do
+ActiveRecord::Schema.define(version: 2022_09_08_035152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "cityName"
+    t.bigint "state_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["state_id"], name: "index_cities_on_state_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "userRole"
@@ -47,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_09_08_034938) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "cities", "states"
   add_foreign_key "userroles", "roles"
   add_foreign_key "userroles", "users"
 end
