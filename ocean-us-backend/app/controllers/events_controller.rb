a0f1274ca25863bed56ecc54d55d899d84d82943
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-11 18:26:06 -0400

    add events strong params

--- ocean-us-backend/app/controllers/events_controller.rb
+++ ocean-us-backend/app/controllers/events_controller.rb
@@ -6,12 +6,30 @@ class EventsController < ApplicationController
     end
 
     def create
-        event = Event.create(even_params)
+        event = Event.create(event_params)
         if event.save
             render json: event, status: :created
         else
             render json: event.errors, status: :unprocessable_entity
         end
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(
+            :address, 
+            :zipcode, 
+            :image, 
+            :like, 
+            :date, 
+            :eventDescription, 
+            :title, 
+            :longitude, 
+            :latitude, 
+            :city_id, 
+            :user_id
+        )
     end
 
 end
