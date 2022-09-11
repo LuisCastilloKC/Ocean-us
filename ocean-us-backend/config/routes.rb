697f58b644684c83450f99e0aa87a72d86baad26
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-11 18:32:47 -0400

    add events route

--- ocean-us-backend/config/routes.rb
+++ ocean-us-backend/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :states, only: [:index, :create]
   resources :cities, only: [:index, :create]
+  resources :events, only: [:index, :create]
 end
