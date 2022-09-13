c9d8ee19e40f04ef819b141a2ca4216a487e6496
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-12 20:58:23 -0400

    add users route

--- ocean-us-backend/config/routes.rb
+++ ocean-us-backend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create]
+  post "/login", to: "users#login"
+  get "auto_login", to: "users#auto_login"
+
   resources :states, only: [:index, :create]
   resources :cities, only: [:index, :create]
   resources :events, only: [:index, :create]
