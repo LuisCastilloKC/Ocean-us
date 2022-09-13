b7620ac34c3b310aec32246bbeb032a0cbf56c2e
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-12 21:55:23 -0400

    add user Controller,seed user data,  add a few method in application controller for JWT

--- ocean-us-backend/app/models/user.rb
+++ ocean-us-backend/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :userroles
     has_many :events
     has_many :cities, through: :events
+
+    # has_secure_password come from Bcryp, this run a few method that are in ApplicationRecord
+    has_secure_password
 end
--- ocean-us-backend/db/seeds.rb
+++ ocean-us-backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user1 = User.create(firstName: "luis", lastName: "Castillo", email: "mr.luiscastillo@gmail.com", password: "123", active: true)
