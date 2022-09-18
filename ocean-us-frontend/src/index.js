efeafc953dc7e80147b84f39d06a69f50d6fd007
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-17 23:59:29 -0400

    importing for redux setup

--- ocean-us-frontend/src/index.js
+++ ocean-us-frontend/src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore , applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+ 
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
