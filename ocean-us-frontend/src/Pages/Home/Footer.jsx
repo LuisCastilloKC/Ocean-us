16e225e5f44351308320f7910b484fcc0690135c
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-17 23:42:07 -0400

    install redux, thunk, react-router-dom

--- ocean-us-frontend/src/Pages/Home/Footer.jsx
+++ ocean-us-frontend/src/Pages/Home/Footer.jsx
@@ -1,19 +1,63 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-
-    return(
+  return (
+    <div>
+      <div>
+        <h1>THINGS TO REDUCE WATER POLLUTION</h1>
+      </div>
+      <div>
         <div>
-            <div>
-                <h1>THINGS  TO REDUCE WATER POLLUTION</h1>
-            </div>
-            <div>
-                <div> 
-                    <h2>Pick up litter</h2>
-                    </div>
-            </div>
+          <h2>1. Pick up litter</h2>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor rhoncus dolor purus non enim praesent elementum
+              facilisis leo, vel fringilla est ullamcorper eget nulla facilisi
+              etiam dignissim diam quis.
+            </p>
+          </div>
         </div>
-    )
-}
+        <div>
+          <h2>2. Never buy bottled water</h2>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor rhoncus dolor purus non enim praesent elementum
+              facilisis leo, vel fringilla est ullamcorper eget nulla facilisi
+              etiam dignissim diam quis.
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2>3. Clean Up Beaches and Rivers</h2>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor rhoncus dolor purus non enim praesent elementum
+              facilisis leo, vel fringilla est ullamcorper eget nulla facilisi
+              etiam dignissim diam quis.
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2>4. Reduce waste</h2>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor rhoncus dolor purus non enim praesent elementum
+              facilisis leo, vel fringilla est ullamcorper eget nulla facilisi
+              etiam dignissim diam quis.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
