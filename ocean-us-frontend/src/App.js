43f05e795bfc70776612a3bd59da6d701071ad50
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-18 23:56:34 -0400

    add Navbar component

--- ocean-us-frontend/src/App.js
+++ ocean-us-frontend/src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar/Navbar";
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Welcome Ocean US</h1>
     </div>
   );
---
+++ ocean-us-frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h1>OCEAN US</h1>
+        <div>
+          <div>
+            <h3>Login</h3>
+          </div>
+          <div>
+            <h3>Sign Up</h3>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
