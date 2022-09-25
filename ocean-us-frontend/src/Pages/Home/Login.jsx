d676fae303e804b029fdacc203ee1c7332f5d65b
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-24 23:26:38 -0400

    add state to Login

---
+++ ocean-us-frontend/src/Pages/Home/Login.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const initialState = {
+    email: "",
+    password: ""
+}
+
+const Login = () => {
+    const [formValue, setFormValue] = useState(initialState)
+    const  { email, password } = formmValue
+     
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Login
