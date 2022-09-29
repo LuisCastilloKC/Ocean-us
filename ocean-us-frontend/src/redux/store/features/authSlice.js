6b82654d85f54459f3e399f8914ca683dc1d438a
Luis Castillo <mr.luiscastillo@gmail.com>
2022-09-29 00:20:41 -0400

    add dispatch data to login and add extraReducers

--- ocean-us-frontend/src/redux/store/features/authSlice.js
+++ ocean-us-frontend/src/redux/store/features/authSlice.js
@@ -1,8 +1,13 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import * as api from "../api"
 
-export const login = createAsyncThunk("auth/login", async() => {
+export const login = createAsyncThunk("auth/login", async({formValue, navigate, toast}) => {
   try {
-    
+    const response = await api.signIn(formValue);
+    toast.success("Login Successfull");
+    navigate("/")
+    ///response.data create 3 promise lifecicle (pending, fulfilled, and rejected)
+    return response.data
   } catch (error) {
     console.log(error)
   }
@@ -15,6 +20,20 @@ const authSlice = createSlice({
     error: "",
     loading: false,
   },
+  extraReducers: {
+    [login.pending]: (state, action) => {
+      state.loading = true
+    },
+    [login.fulfilled]: (state, action) => {
+      state.loading = false
+      localStorage.setItem("profile", JSON.stringify({...action.payload}))
+      state.user = action.payload
+    },
+    [login.rejected]: (state, action) => {
+      state.loading = false 
+      state.error = action.payload.message
+    },
+  }
 });
 
 export default authSlice.reducer;
