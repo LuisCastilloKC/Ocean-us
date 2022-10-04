14aea564cb78ebb524a18f9b4c4e3284245e10c0
Luis Castillo <mr.luiscastillo@gmail.com>
2022-10-03 23:37:40 -0400

    add signup route and signup API endpoint

--- ocean-us-frontend/src/redux/store/api.js
+++ ocean-us-frontend/src/redux/store/api.js
@@ -3,3 +3,4 @@ import axios from "axios";
 const API = axios.create({baseURL: "http://localhost:3000"})
 
 export const signIn = (formData) => API.post("/login", formData)
+export const signUp = (formData) => API.post("/signup", formData)
